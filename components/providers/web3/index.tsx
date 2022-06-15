47c567cd6b4b7ae09229bbfa90d6f0a06beea29a
Sarmun Bustillo <sarmun.b@gmail.com>
2022-06-15 15:14:13 +0200

    Create Provider context

---
+++ components/providers/web3/index.tsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from 'react';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const Web3Context = createContext<any>(null);
+
+const Web3Provider: React.FC<Props> = ({ children }) => {
+    const [web3Api, setWeb3Api] = useState({ test: 'hello provider' });
+    return (
+        <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
+    );
+};
+
+export function useWeb3() {
+    return useContext(Web3Context);
+}
+export default Web3Provider;
