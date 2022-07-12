0f2abb5ad308dc05142d235d4e4ca552f1a2e745
Sarmun Bustillo <sarmun.b@gmail.com>
2022-07-12 22:27:04 +0200

    UseListed hook

--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -1,10 +1,15 @@
 import { Web3Dependencies } from '@_types/hooks';
 import { hookFactory as createAccountHook, UseAccountHook } from './useAccount';
 import { hookFactory as createNetworkHook, UseNetworkHook } from './useNetwork';
+import {
+    hookFactory as createListedNftsHook,
+    UseListedNftsHook,
+} from './useListedNfts';
 
 export type Web3Hooks = {
     useAccount: UseAccountHook;
     useNetwork: UseNetworkHook;
+    useListedNfts: UseListedNftsHook;
 };
 
 export type SetupHooks = {
@@ -15,5 +20,6 @@ export const SetupHooks: SetupHooks = (dependencies) => {
     return {
         useAccount: createAccountHook(dependencies),
         useNetwork: createNetworkHook(dependencies),
+        useListedNfts: createListedNftsHook(dependencies),
     };
 };
